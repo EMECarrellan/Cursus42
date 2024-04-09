c9fab1eaa9c111ab2fb09b44badf2bc0f875b6af
EMECarrellan <105559539+EMECarrellan@users.noreply.github.com>
2024-04-09 18:26:10 +0200

    edit libft + isalnum

--- libft.h
+++ libft.h
@@ -1,4 +1,21 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+
+int	ft_isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
+int	ft_isalpha(int c)
+{
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
 int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
 }
+
+#endif
